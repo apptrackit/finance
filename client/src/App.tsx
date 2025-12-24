e874861ea7413fa9f0b1449704d37d3769c22942
Botond Balla <ballabotond@ballabotond.com>
2025-12-24 08:32:00 +0100

    version bump

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,7 +10,7 @@ import { usePrivacy } from './context/PrivacyContext'
 import { startOfMonth, endOfMonth, format } from 'date-fns'
 
 
-const APP_VERSION = '0.9.4'
+const APP_VERSION = '0.9.5'
 
 
 type Account = {
