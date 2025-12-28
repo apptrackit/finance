447801747882c12919ce3b2b3281c516f087d2a1
31b4 <szilagyibence@szilagyibence.com>
2025-12-28 12:36:18 +0100

    v1.0.0 version bump

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,7 +10,7 @@ import { usePrivacy } from './context/PrivacyContext'
 import { startOfMonth, endOfMonth, format } from 'date-fns'
 
 
-const APP_VERSION = '0.9.7'
+const APP_VERSION = '1.0.0'
 
 
 type Account = {
