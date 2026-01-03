631b6e96ef9dd8b8722a24adaacb8f03abf28e3c
31b4 <szilagyibence@szilagyibence.com>
2026-01-03 01:09:47 +0100

    v1.0.3 bump

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,7 +10,7 @@ import { usePrivacy } from './context/PrivacyContext'
 import { startOfMonth, endOfMonth, format } from 'date-fns'
 
 
-const APP_VERSION = '1.0.2'
+const APP_VERSION = '1.0.3'
 
 
 type Account = {
